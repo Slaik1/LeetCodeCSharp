751632d6068e275f8c603b62fb18e80c5f50d18e
Slaik <marinakolmik@gmail.com>
2023-04-07 23:35:36 +0300

    Add 1.TwoSum

---
+++ LeetCodeCSharp/1.TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace LeetCodeCsharp
+{
+    public class TwoSumSolution
+    {
+        static void Main()
+        { }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
